c9f4e9ca8776157780e86f5d6c538816c92f62ce
Htethsu176 <htethsunaychi176@gmail.com>
2023-12-13 15:47:03 +0630

    read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -1,24 +1,31 @@
 import csv
-from collections import defaultdict
 
-def split_csv(input_file, column_index):
-    data_dict = defaultdict(list)
-    
-    with open(input_file,'r') as file:
-        reader = csv.reader(file)
-        header = next(reader)
+def split_csv(input_file, column_name):
+    data_dict = {}
+
+    with open(input_file, newline='') as file:
+        reader = csv.DictReader(file)
+        header = reader.fieldnames
 
         for row in reader:
-            print(row)
-            value = row[column_index]
-            data_dict[value].append(row)
+            value = row[column_name]
+
+            if value in data_dict:
+                data_dict[value].append(row)
+            else:
+                data_dict[value] = [row]
 
         for value, rows in data_dict.items():
-            output_file = f'{header[column_index]}_{value}.csv'
-            with open(output_file, 'w') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow(header)
+            output_file = f'{column_name}_{value}.csv'
+            with open(output_file, 'w', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames = header)
+                writer.writeheader()
                 writer.writerows(rows)
+def main():
+    input_file = '/home/ubuntu/Downloads/ap_ssid_status_20230822_103752.csv'
+    column_name = 'model'
 
-split_csv('/home/ubuntu/Downloads/ap_ssid_status_20230822_103752.csv',3)
-
+    split_csv(input_file, column_name)
+    
+if __name__ == "__main__":
+    main()
